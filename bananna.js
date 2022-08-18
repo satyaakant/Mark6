49d81f39ff5a7f11619db7082ba1e19d5a6569db
satyaaa <satyakantmishra50@gmail.com>
2022-08-18 18:25:04 +0530

    css and server
    connected app with a server  and added css stylesheet to make the app beautiful

--- bananna.js
+++ bananna.js
@@ -2,7 +2,24 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var output = document.querySelector(".output");
 
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
+
+function geturl(text){
+    return serverurl + "?" + "text=" + text
+}
+ 
+function errorrhandler(error){
+    console.log("error occured ", error);
+    alert("there is error in server please try again");
+}
+
 function clickhandler(){
-    output.innerText="me aste nu be" + txtinput.value;
+      var answer= txtinput.value;
+      fetch(geturl(answer))
+      .then(response => response.json())
+      .then(json => {
+        output.innerText= json.contents.translated;
+      })
+      .catch(errorrhandler)
 };
 btnTranslate.addEventListener("click", clickhandler)
